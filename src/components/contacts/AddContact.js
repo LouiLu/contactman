859b3a21b205bcb987edb9317df312bd48166897
Smallulu <junhaol1206@gmail.com>
2019-05-03 11:16:50 -0700

    tidy textinput to components and validation added

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Consumer } from '../context';
 import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
 
 class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onChange = e => {
@@ -17,6 +19,20 @@ class AddContact extends Component {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+    // validation for invalid input in form
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required!' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required!' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required!' } });
+      return;
+    }
+
     const newContact = {
       id: uuid(),
       name,
@@ -31,7 +47,7 @@ class AddContact extends Component {
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
     return (
       <Consumer>
         {value => {
@@ -42,39 +58,31 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name" />
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Enter name..."
-                      className="form-control form-control-lg"
-                      value={name}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email" />
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter email..."
-                      className="form-control form-control-lg"
-                      value={email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone" />
-                    <input
-                      type="text"
-                      name="phone"
-                      placeholder="Enter phone..."
-                      className="form-control form-control-lg"
-                      value={phone}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter name..."
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email..."
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter phone number..."
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
                   <input
                     type="submit"
                     value="Add Contact"
